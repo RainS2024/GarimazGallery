46e41f94c67f1d1334439b8d1a676c6eeeed8d6f
Garima S <garimas@tekarch.com>
2025-05-28 12:29:17 -0700

    added watermarks

--- src/app/component/Cards/cards.component.ts
+++ src/app/component/Cards/cards.component.ts
@@ -11,63 +11,63 @@ export class CardsComponent {
  cards = [
     {
       image: 'https://garimas-gallery-images.s3.us-west-1.amazonaws.com/Cards/SG-1.png',
-      alt: 'Greeting Card 1', code: 'SG-1',isPremium: false,
+      alt: 'Greeting Card 1', code: 'ShriGanesh-1',isPremium: false,
     },
     {
       image: 'https://garimas-gallery-images.s3.us-west-1.amazonaws.com/Cards/BD-1.png',
-      alt: 'Greeting Card 2', code: 'BD-1',isPremium: false,
+      alt: 'Greeting Card 2', code: 'Birthday-1',isPremium: false,
     },
     {
       image: 'https://garimas-gallery-images.s3.us-west-1.amazonaws.com/Cards/BD-2.png',
-      alt: 'Greeting Card 3', code: 'BD-2',isPremium: false,
+      alt: 'Greeting Card 3', code: 'Birthday-2',isPremium: false,
     },
     {
       image: 'https://garimas-gallery-images.s3.us-west-1.amazonaws.com/Cards/FL-1.png',
-      alt: 'Greeting Card 4', code: 'FL-1',isPremium: false,
+      alt: 'Greeting Card 4', code: 'Flower-1',isPremium: false,
     },
     {
       image: 'https://garimas-gallery-images.s3.us-west-1.amazonaws.com/Cards/FL-2.png',
-      alt: 'Greeting Card 5', code: 'FL-2',isPremium: false,
+      alt: 'Greeting Card 5', code: 'Flower-2',isPremium: false,
     },
     {
       image: 'https://garimas-gallery-images.s3.us-west-1.amazonaws.com/Cards/FL-3.png',
-      alt: 'Greeting Card 6', code: 'FL-3',isPremium: false,
+      alt: 'Greeting Card 6', code: 'Flower-3',isPremium: false,
     },
     {
       image: 'https://garimas-gallery-images.s3.us-west-1.amazonaws.com/Cards/FL-4.png',
-      alt: 'Greeting Card 7', code: 'FL-4',isPremium: false,
+      alt: 'Greeting Card 7', code: 'Flower-4',isPremium: false,
     },
     {
       image: 'https://garimas-gallery-images.s3.us-west-1.amazonaws.com/Cards/SC-1.png',
-      alt: 'Greeting Card 8', code: 'SC-1',isPremium: false,
+      alt: 'Greeting Card 8', code: 'Scene-1',isPremium: false,
     },
     {
       image: 'https://garimas-gallery-images.s3.us-west-1.amazonaws.com/Cards/SC-2.png',
-      alt: 'Greeting Card 9', code: 'SC-2',isPremium: false,
+      alt: 'Greeting Card 9', code: 'Scene-2',isPremium: false,
     },
     {
       image: 'https://garimas-gallery-images.s3.us-west-1.amazonaws.com/Cards/MD.png',
-      alt: 'Greeting Card 10', code: 'MD',isPremium: false,
+      alt: 'Greeting Card 10', code: 'MothersDay',isPremium: false,
     },
     {
       image: 'https://garimas-gallery-images.s3.us-west-1.amazonaws.com/Cards/HRT-1.png',
-      alt: 'Greeting Card 11', code: 'HRT-1',isPremium: false,
+      alt: 'Greeting Card 11', code: 'Heart-1',isPremium: false,
     },
     {
       image: 'https://garimas-gallery-images.s3.us-west-1.amazonaws.com/Cards/HRT-2.png',
-      alt: 'Greeting Card 12', code: 'HRT-2',isPremium: false,
+      alt: 'Greeting Card 12', code: 'Heart-2',isPremium: false,
     },
     {
       image: 'https://garimas-gallery-images.s3.us-west-1.amazonaws.com/Cards/GR-1.png',
-      alt: 'Greeting Card 13', code: 'GR-1',isPremium: false,
+      alt: 'Greeting Card 13', code: 'Graduation-1',isPremium: false,
     },
     {
       image: 'https://garimas-gallery-images.s3.us-west-1.amazonaws.com/Cards/GR-2.png',
-      alt: 'Greeting Card 14', code: 'GR-2',isPremium: false,
+      alt: 'Greeting Card 14', code: 'Graduation-2',isPremium: false,
     },
     {
       image: 'https://garimas-gallery-images.s3.us-west-1.amazonaws.com/Cards/PR-1.jpg',
-      alt: 'Greeting Card 15', code: 'PR-1',isPremium: true,
+      alt: 'Greeting Card 15', code: 'Premium-1',isPremium: true,
     },
     // Add more card objects here
   ];
